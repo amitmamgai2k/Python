558226c8a73e8b87be3d56c3b04b788285dde015
amitmamgai2k <123645217+amitmamgai2k@users.noreply.github.com>
2025-04-24 15:56:47 +0530

    Chapter 1 Complete

---
+++ Chapter1.py
@@ -0,0 +1,34 @@
+# Print in Python
+
+print("Hello World")
+
+#Variables & Data Types in Python
+
+a = 10 #int
+b = 3.14 #float
+c = "Hello" #string
+d = True    #bool
+print(type(a), type(b), type(c), type(d)) #<class 'int'> <class 'float'> <class 'str'> <class 'bool'>
+
+#Operators in Python
+
+a = 10
+b = 5
+print(a + b) #15
+print(a - b) #5
+print(a * b) #50
+print(a / b) #2.0
+print(a % b) #0
+print(a ** b) #100000
+print(a // b) #2
+print(a == b) #False
+print(a != b) #True
+print(a > b) #True
+print(a < b) #False
+print(a >= b) #True
+print(a <= b) #False
+
+#Input & Output in Python
+
+name = input("Enter your name: ")
+print("Hello", name)
